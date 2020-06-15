d5389f31b8f4590a7b211bf37eda4683c4948ddc
Nurizko Maulana <nurizko.maulana@gmail.com>
2020-06-15 14:33:08 +0700

    start first source

---
+++ login/index.php
@@ -0,0 +1,162 @@
+<?php
+session_start();
+
+require_once "Auth.php";
+require_once "Util.php";
+
+$auth = new Auth();
+$db_handle = new DBController();
+$util = new Util();
+
+require_once "authCookieSessionValidate.php";
+
+// if ($isLoggedIn) {
+// 	echo "islogin";
+//     $util->redirect("../examples/dashboard.php");
+// }
+
+if (! empty($_POST["login"])) {
+    $isAuthenticated = false;
+    
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    
+    $user = $auth->getMemberByUsername($username);
+    if (password_verify($password, $user[0]["password"])) {
+        $isAuthenticated = true;
+    }
+    
+    if ($isAuthenticated) {
+        $_SESSION["member_id"] = $user[0]["user_id"];
+        
+        // Set Auth Cookies if 'Remember Me' checked
+        if (! empty($_POST["remember"])) {
+            setcookie("member_login", $username, $cookie_expiration_time);
+            
+            $random_password = $util->getToken(16);
+            setcookie("random_password", $random_password, $cookie_expiration_time);
+            
+            $random_selector = $util->getToken(32);
+            setcookie("random_selector", $random_selector, $cookie_expiration_time);
+            
+            $random_password_hash = password_hash($random_password, PASSWORD_DEFAULT);
+            $random_selector_hash = password_hash($random_selector, PASSWORD_DEFAULT);
+            
+            $expiry_date = date("Y-m-d H:i:s", $cookie_expiration_time);
+            
+            // mark existing token as expired
+            $userToken = $auth->getTokenByUsername($username, 0);
+            if (! empty($userToken[0]["id"])) {
+                $auth->markAsExpired($userToken[0]["id"]);
+            }
+            // Insert new token
+            $auth->insertToken($username, $random_password_hash, $random_selector_hash, $expiry_date);
+        } else {
+            $util->clearAuthCookie();
+        }
+        $util->redirect("../examples/dashboard.php");
+    } else {
+		echo "Invalid Login";
+		$message = "Invalid Login";
+		?>
+		<script type="text/javascript">
+			console.log("Invalid Login");
+		</script>
+		<?php
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Login V11</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="css/util.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+<!--===============================================================================================-->
+</head>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100 p-l-50 p-r-50 p-t-77 p-b-30">
+				<form class="login100-form validate-form" action="" method="post" id="frmLogin">
+					<span class="login100-form-title p-b-55">
+						Login
+					</span>
+
+					<div class="wrap-input100 validate-input m-b-16" data-validate = "Valid email is required: ex@abc.xyz">
+						<input class="input100" type="text" name="username" placeholder="Username" value="<?php if(isset($_COOKIE["member_login"])) { echo $_COOKIE["member_login"]; } ?>">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<span class="lnr lnr-users"></span>
+						</span>
+					</div>
+
+					<div class="wrap-input100 validate-input m-b-16" data-validate = "Password is required">
+						<input class="input100" type="password" name="password" placeholder="Password" value="<?php if(isset($_COOKIE["member_password"])) { echo $_COOKIE["member_password"]; } ?>">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<span class="lnr lnr-lock"></span>
+						</span>
+					</div>
+
+					<div class="contact100-form-checkbox m-l-4">
+						<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember"
+						<?php if(isset($_COOKIE["member_login"])) { ?> checked <?php } ?>>
+						<label class="label-checkbox100" for="ckb1">
+							Remember me
+						</label>
+					</div>
+					
+					<div class="container-login100-form-btn p-t-25">
+						<button class="login100-form-btn" type="submit" name="login" value="Login">
+							Login
+						</button>
+					</div>
+
+					<div class="text-center w-full p-t-115">
+						<span class="txt1">
+							Not a member?
+						</span>
+
+						<a class="txt1 bo1 hov1" href="#">
+							Sign up now							
+						</a>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+	
+
+	
+<!--===============================================================================================-->	
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/bootstrap/js/popper.js"></script>
+	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<!--===============================================================================================-->
+	<script src="vendor/select2/select2.min.js"></script>
+<!--===============================================================================================-->
+	<script src="js/main.js"></script>
+
+</body>
+</html>
