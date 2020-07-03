57556a727d900a3e0a9994844de37caf525e1518
Nurizko Maulana <nurizko.maulana@gmail.com>
2020-07-03 14:32:04 +0700

    add readme file

--- database/accommodation_db.sql
+++ database/accommodation_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 15, 2020 at 10:27 PM
+-- Generation Time: Jul 03, 2020 at 09:30 AM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.6
 
@@ -42,17 +42,163 @@ INSERT INTO `admin` (`user_id`, `admin_name`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `block`
+--
+
+CREATE TABLE `block` (
+  `block_id` int(11) NOT NULL,
+  `block_name` varchar(5) NOT NULL,
+  `collage_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `block`
+--
+
+INSERT INTO `block` (`block_id`, `block_name`, `collage_id`) VALUES
+(1, 'XC1', 1),
+(2, 'XC2', 1),
+(3, 'XB1', 1),
+(4, 'XB2', 1),
+(7, 'W1', 2),
+(8, 'W2', 2),
+(11, 'W3', 2),
+(12, 'WA1', 2),
+(17, 'WA3', 2),
+(20, 'U5', 3),
+(21, 'U6', 3),
+(22, 'S04', 4),
+(25, 'S05', 4),
+(29, 'S08', 4),
+(30, 'S09', 4),
+(33, 'S10', 4),
+(34, 'S11', 4),
+(37, 'S12', 4),
+(38, 'S13', 4),
+(41, 'S14', 4),
+(42, 'S15', 4),
+(45, 'S20', 4),
+(46, 'S22', 4),
+(49, 'S23', 4),
+(50, 'S24', 4),
+(53, 'S37', 4),
+(54, 'S38', 4),
+(57, 'S39', 4),
+(58, 'S40', 4),
+(63, 'K04', 5),
+(64, 'K06', 5),
+(67, 'K09', 5),
+(68, 'K13', 5),
+(71, 'K14', 5),
+(72, 'K15', 5),
+(75, 'K16', 5),
+(76, 'K23', 5),
+(79, 'K25', 5),
+(80, 'M05', 6),
+(83, 'M06', 6),
+(84, 'M07', 6),
+(87, 'M09', 6),
+(88, 'M10', 6),
+(91, 'M11', 6),
+(92, 'M16', 6),
+(95, 'M17', 6),
+(96, 'M19', 6),
+(97, 'M20', 6),
+(98, 'M21', 6),
+(99, 'H02', 7),
+(100, 'H03', 7),
+(101, 'H04', 7),
+(102, 'H05', 7),
+(103, 'H06', 7),
+(104, 'H07', 7),
+(105, 'H08', 7),
+(106, 'H09', 7),
+(107, 'H10', 7),
+(108, 'H11', 7),
+(109, 'H12', 7),
+(110, 'H13', 7),
+(111, 'H14', 7),
+(112, 'H15', 7),
+(113, 'H16', 7),
+(114, 'H17', 7),
+(115, 'H18', 7),
+(116, 'H19', 7),
+(117, 'H20', 7),
+(118, 'H21', 7),
+(119, 'H22', 7),
+(120, 'H23', 7),
+(121, 'H24', 7),
+(122, 'G09', 8),
+(123, 'G10', 8),
+(124, 'G11', 8),
+(125, 'G13', 8),
+(126, 'G14', 8),
+(127, 'G15', 8),
+(128, 'G16', 8),
+(129, 'G17', 8),
+(130, 'G18', 8),
+(131, 'G19', 8),
+(132, 'G20', 8),
+(133, 'G21', 8),
+(134, 'G22', 8),
+(135, 'G23', 8),
+(136, 'G24', 8),
+(137, 'G25', 8),
+(138, 'G28', 8),
+(139, 'UA1', 9),
+(140, 'UA2', 9),
+(141, 'UB1', 9),
+(142, 'UB2', 9),
+(143, 'L17', 10),
+(144, 'L18', 10),
+(145, 'L20', 10),
+(146, 'L21', 10),
+(147, 'L22', 10),
+(148, 'L23', 10),
+(149, 'L24', 10),
+(150, 'L25', 10),
+(151, 'L26', 10),
+(152, 'L27', 10),
+(153, 'L28', 10),
+(154, 'L29', 10),
+(155, 'L31', 10),
+(156, 'L32', 10),
+(157, 'L33', 10),
+(158, 'L34', 10),
+(159, 'L35', 10),
+(160, 'L36', 10),
+(161, 'L37', 10),
+(162, 'L38', 10);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `booking`
 --
 
 CREATE TABLE `booking` (
   `book_id` int(11) NOT NULL,
-  `date_booked` date NOT NULL,
-  `total_price` decimal(10,2) NOT NULL,
+  `date_booked` timestamp NOT NULL DEFAULT current_timestamp(),
+  `total_price` decimal(10,2) NOT NULL DEFAULT 2000.00,
   `book_status` varchar(20) NOT NULL,
-  `room_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL
+  `room_id` int(11) NOT NULL DEFAULT 1,
+  `room_no` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `user_name` varchar(100) NOT NULL,
+  `matrix` varchar(10) NOT NULL,
+  `college` varchar(11) NOT NULL,
+  `block` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `booking`
+--
+
+INSERT INTO `booking` (`book_id`, `date_booked`, `total_price`, `book_status`, `room_id`, `room_no`, `user_id`, `user_name`, `matrix`, `college`, `block`) VALUES
+(1, '2020-07-02 05:55:58', '2000.00', 'approve', 1, 161, 4, 'Nurizko Maulana', 'A18CS0318', 'KDOJ', 'XC1'),
+(2, '2020-07-02 05:58:46', '2000.00', 'approve', 2, 244, 3, 'Edowardo', 'A18CS0297', 'KDOJ', 'XC2'),
+(4, '2020-07-03 00:50:47', '2000.00', 'approve', 1, 323, 4, 'nurizko maulana', 'A18CS0318', 'KDOJ', 'XC1'),
+(5, '2020-07-03 01:04:29', '2000.00', 'rejected', 1, 12, 4, 'raihan', 'A18CS0318', 'KDOJ', 'XC1');
 
 -- --------------------------------------------------------
 
@@ -63,20 +209,24 @@ CREATE TABLE `booking` (
 CREATE TABLE `college` (
   `college_id` int(11) NOT NULL,
   `college_name` varchar(50) NOT NULL,
-  `college_address` varchar(50) DEFAULT NULL
+  `college_price` double DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `college`
 --
 
-INSERT INTO `college` (`college_id`, `college_name`, `college_address`) VALUES
-(1, 'KDOJ (Kolej Dato Onn Jafar)', NULL),
-(2, 'KDSE (Kolej Datin Seri Endon)', NULL),
-(3, 'KP (Kolej Perdana)', NULL),
-(4, 'KTC (Kolej Tuanku Canselor)', NULL),
-(5, 'KTR (Kolej Tun Razak)', NULL),
-(6, 'KTDI (Kolej Tun Dr Ismail)', NULL);
+INSERT INTO `college` (`college_id`, `college_name`, `college_price`) VALUES
+(1, 'KDOJ (Kolej Dato Onn Jafar)', 1000),
+(2, 'KDSE (Kolej Datin Seri Endon)', 800),
+(3, 'KP (Kolej Perdana)', 600),
+(4, 'KTC (Kolej Tuanku Canselor)', 1000),
+(5, 'KTR (Kolej Tun Razak)', 800),
+(6, 'KTDI (Kolej Tun Dr Ismail)', 600),
+(7, 'KTF (Kolej Tun Fatimah)', 1000),
+(8, 'KRP (Kolej Rahman Putra)', 800),
+(9, 'K9/10 (Kolej 9/10)', 600),
+(10, 'KTHO (Kolej Tun Hussein On)', 1000);
 
 -- --------------------------------------------------------
 
@@ -103,6 +253,14 @@ CREATE TABLE `room` (
   `college_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dumping data for table `room`
+--
+
+INSERT INTO `room` (`room_id`, `room_no`, `room_type`, `room_price`, `college_id`) VALUES
+(1, 161, 'single', '2000.00', 1),
+(2, 162, 'single', '2000.00', 1);
+
 -- --------------------------------------------------------
 
 --
@@ -121,7 +279,8 @@ CREATE TABLE `student` (
 
 INSERT INTO `student` (`user_id`, `student_name`, `matrix`) VALUES
 (3, 'Nurizko Maulana', 'A18CS0318'),
-(4, 'Edowardo', 'A18CS0297');
+(4, 'Edowardo', 'A18CS0297'),
+(6, 'Raihan Wilinio', 'A18CS0316');
 
 -- --------------------------------------------------------
 
@@ -174,10 +333,12 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`user_id`, `username`, `password`, `level`, `status`) VALUES
-(1, 'admin', '$2a$10$0FHEQ5/cplO3eEKillHvh.y009Wsf4WCKvQHsZntLamTUToIBe.fG', 'admin', 1),
+(1, 'admin', '$2y$10$CgiUAXG7C8Ekj6zPqvQIOO0PlCPsZMrjmMf0kKdLk7owDashctmPq', 'admin', 1),
 (3, 'maulana2000', '$2a$10$0FHEQ5/cplO3eEKillHvh.y009Wsf4WCKvQHsZntLamTUToIBe.fG', 'student', 1),
 (4, 'edowardo', '$2a$10$0FHEQ5/cplO3eEKillHvh.y009Wsf4WCKvQHsZntLamTUToIBe.fG', 'student', 1),
-(5, 'manager', '$2a$10$0FHEQ5/cplO3eEKillHvh.y009Wsf4WCKvQHsZntLamTUToIBe.fG', 'manager', 1);
+(5, 'manager', '$2a$10$0FHEQ5/cplO3eEKillHvh.y009Wsf4WCKvQHsZntLamTUToIBe.fG', 'manager', 1),
+(6, 'raihan', '$2y$10$FCaCpUrWXTLWoTUI5Ke64eaoYaddz273SwNOjrylrBglwqMtIq2mW', 'student', 0),
+(7, 'mokhtar', '$2y$10$qcIbVaLgsKmAqkkG8J1pW.ZxrAnhegf6Sd95.TKlHBARVku9vclkq', 'student', 1);
 
 --
 -- Indexes for dumped tables
@@ -188,6 +349,13 @@ INSERT INTO `user` (`user_id`, `username`, `password`, `level`, `status`) VALUES
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `block`
+--
+ALTER TABLE `block`
+  ADD PRIMARY KEY (`block_id`),
+  ADD KEY `collage_id` (`collage_id`);
 
 --
 -- Indexes for table `booking`
@@ -239,16 +407,28 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `block`
+--
+ALTER TABLE `block`
+  MODIFY `block_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=163;
+
+--
+-- AUTO_INCREMENT for table `booking`
+--
+ALTER TABLE `booking`
+  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT for table `college`
 --
 ALTER TABLE `college`
-  MODIFY `college_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `college_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `room`
 --
 ALTER TABLE `room`
-  MODIFY `room_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `room_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tbl_token_auth`
@@ -260,7 +440,7 @@ ALTER TABLE `tbl_token_auth`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
@@ -271,6 +451,12 @@ ALTER TABLE `user`
 --
 ALTER TABLE `admin`
   ADD CONSTRAINT `admin_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+--
+-- Constraints for table `block`
+--
+ALTER TABLE `block`
+  ADD CONSTRAINT `block_ibfk_1` FOREIGN KEY (`collage_id`) REFERENCES `college` (`college_id`);
 
 --
 -- Constraints for table `booking`
---
+++ readme.txt
@@ -0,0 +1,20 @@
+Group name : InfitiTea
+Member name :
+1. Nurizko Maulana
+2. Edowardo
+3. Raigan Muhammad Wilinio
+4. Mokhtar
+
+Step to install database
+upload sql dump file from folder database and inport it to mysql with database name accommodation_db
+
+username and password for the web login
+1. For admin
+	- uname : admin
+	- pass	: admin
+2. For manager
+	- uname : manager
+	- pass	: manager
+3. For student 
+	- uname : edowardo
+	- pass	: admin
